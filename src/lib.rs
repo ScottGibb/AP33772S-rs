6559f063f6e45fcee28e06540e0cadabd9d8571e
Scott Gibb <smgibb@yahoo.com>
2025-05-21 23:23:29 +0100

    adding placeholder text

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! This is a placeholder crate to reserve the `ap33772` namespace.
+//! Actual functionality will be published in a future release.
