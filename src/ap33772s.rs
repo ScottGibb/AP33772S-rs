16875a348fc7d7b2f762854fa4da0d2bb07c337c
Scott Gibb <smgibb@yahoo.com>
2025-05-28 22:39:45 +0100

    adding updated getters

--- src/ap33772s.rs
+++ src/ap33772s.rs
@@ -1,5 +1,21 @@
 use super::hal::*;
+use uom::si::f32::Power;
+use uom::si::f32::ThermodynamicTemperature;
+use uom::si::f32::ElectricPotential;
+use uom::si::f32::ElectricCurrent;
 
+
+#[derive(Debug)]
+pub struct AP33772SStatistics{
+    pub current: ElectricCurrent,
+    pub voltage: ElectricPotential,
+    pub power: Power,
+    pub temperature: ThermodynamicTemperature,
+
+    pub requested_voltage: ElectricPotential,
+    pub requested_current: ElectricCurrent,
+    pub requested_power: Power,
+}
 pub struct Ap33772s<I2C: I2c> {
     pub(crate) i2c: I2C,
 }
--- src/getters.rs
+++ src/getters.rs
@@ -1,5 +1,6 @@
 use super::hal::*;
 use crate::Ap33772sError;
+use crate::ap33772s;
 use crate::ap33772s::Ap33772s;
 
 use crate::commands::requested::current_requested::CurrentRequested;
@@ -13,37 +14,76 @@ use uom::si::f32::ElectricCurrent;
 use uom::si::f32::ElectricPotential;
 use uom::si::f32::ThermodynamicTemperature;
 
+/// This module provides methods to read various statistics from the AP33772S device.
+/// It includes methods to get the current, voltage, temperature, power,
 impl<I2C: I2c> Ap33772s<I2C> {
     #[maybe_async::maybe_async]
-    pub async fn get_current(mut self) -> Result<ElectricCurrent, Ap33772sError> {
+    pub async fn get_current(&mut self) -> Result<ElectricCurrent, Ap33772sError> {
         let current = self.read_one_byte_command::<Current>()?;
         Ok(current.current())
     }
 
     #[maybe_async::maybe_async]
-    pub async fn get_voltage(mut self) -> Result<ElectricPotential, Ap33772sError> {
+    pub async fn get_voltage(&mut self) -> Result<ElectricPotential, Ap33772sError> {
         let voltage = self.read_one_byte_command::<Voltage>()?;
         Ok(voltage.voltage())
     }
 
     #[maybe_async::maybe_async]
-    pub async fn get_temperature(mut self) -> Result<ThermodynamicTemperature, Ap33772sError> {
+    pub async fn get_temperature(&mut self) -> Result<ThermodynamicTemperature, Ap33772sError> {
         let temperature = self.read_one_byte_command::<Temperature>()?;
         Ok(temperature.temperature())
     }
+
     #[maybe_async::maybe_async]
-    pub async fn get_requested_voltage(mut self) -> Result<ElectricPotential, Ap33772sError> {
+    pub async fn get_power(mut self) -> Result<uom::si::f32::Power, Ap33772sError> {
+        let current = self.get_current().await?;
+        let voltage = self.get_voltage().await?;
+        let power = current * voltage;
+        Ok(power)
+    }
+    #[maybe_async::maybe_async]
+    pub async fn get_requested_voltage(&mut self) -> Result<ElectricPotential, Ap33772sError> {
         let requested_voltage = self.read_two_byte_command::<VoltageRequested>()?;
         Ok(requested_voltage.voltage())
     }
     #[maybe_async::maybe_async]
-    pub async fn get_requested_current(mut self) -> Result<ElectricCurrent, Ap33772sError> {
+    pub async fn get_requested_current(&mut self) -> Result<ElectricCurrent, Ap33772sError> {
         let requested_current = self.read_two_byte_command::<CurrentRequested>()?;
         Ok(requested_current.current())
     }
     #[maybe_async::maybe_async]
+    pub async fn get_requested_power(&mut self) -> Result<uom::si::f32::Power, Ap33772sError> {
+        let requested_voltage = self.get_requested_voltage().await?;
+        let requested_current = self.get_requested_current().await?;
+        let requested_power = requested_voltage * requested_current;
+        Ok(requested_power)
+    }
+    #[maybe_async::maybe_async]
+    pub async fn get_requested_statistics(
+        &mut self,
+    ) -> Result<ap33772s::AP33772SStatistics, Ap33772sError> {
+        let current = self.get_current().await?;
+        let voltage = self.get_voltage().await?;
+        let temperature = self.get_temperature().await?;
+        let requested_voltage = self.get_requested_voltage().await?;
+        let requested_current = self.get_requested_current().await?;
+        Ok(ap33772s::AP33772SStatistics {
+            current,
+            voltage,
+            temperature,
+            power: current * voltage,
+            requested_voltage,
+            requested_current,
+            requested_power: requested_voltage * requested_current,
+        })
+    }
+}
+
+impl<I2C: I2c> Ap33772s<I2C> {
+    #[maybe_async::maybe_async]
     pub async fn get_minimum_selection_voltage(
-        mut self,
+        &mut self,
     ) -> Result<ElectricPotential, Ap33772sError> {
         let voltage_selection = self.read_one_byte_command::<MinimumSelectionVoltage>()?;
         Ok(voltage_selection.voltage())
