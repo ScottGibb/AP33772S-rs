152942e0c6601d0d0c74293eee19490f67cf016c
Scott Gibb <smgibb@yahoo.com>
2025-08-14 14:36:44 +0200

    AVS Examples checked and working

--- examples/avs_fixed.rs
+++ examples/avs_fixed.rs
@@ -6,9 +6,10 @@ use ap33772s_rs::{
 use utils::setup_delay;
 // This another example inspired by the Centy Labs example for the AP33772S.
 // [Centy Labs Example](https://github.com/CentyLab/AP33772S-CentyLab/blob/main/examples/AVSfixed/AVSfixed.ino)
+// This example is designed to work with the [Anker Prime 27,650mAh Power Bank (250W)](https://www.amazon.co.uk/dp/B0BYP2F3SG?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_1&th=1)
 fn main() {
     let delay = setup_delay();
-    let i2c = utils::setup_i2c(1_0000).expect("Failed to set up I2C");
+    let i2c = utils::setup_i2c(1_000).expect("Failed to set up I2C");
     let mut ap33772s =
         Ap33772s::new_default(i2c, delay).expect("Failed to create AP33772S instance");
 
@@ -25,7 +26,7 @@ fn main() {
     loop {
         let response = ap33772s
             .negotiate_power_delivery(
-                PowerDataObject::ExtendedPowerRange8,
+                PowerDataObject::StandardPowerRange6,
                 Some(adjustable_voltage_supply),
                 CurrentSelection::_3A,
                 &power_data_objects,
