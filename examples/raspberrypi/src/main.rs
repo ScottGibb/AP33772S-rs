7032f3e9c9f6d21ca8352ffa72feabd3961f893a
Scott Gibb <smgibb@yahoo.com>
2025-08-15 16:08:22 +0100

    fixed rust code

--- examples/raspberrypi/src/main.rs
+++ examples/raspberrypi/src/main.rs
@@ -2,16 +2,10 @@
 /// It sets up the I2C communication and sends a power delivery request to the AP33772S device.
 /// The example is designed to run on a Raspberry Pi with the AP33772S
 use ap33772s_rs::ap33772s::Ap33772s;
+use embedded_hal::blocking::delay::DelayMs;
 use rppal::i2c::I2c;
-fn main() {
-    let i2c = I2c::new().expect("Failed to set up I2C");
-    println!(
-        "I2C Clock Speed: {}",
-        i2c.get_clock_speed().expect("Failed to get clock speed"),
-    );
 use std::thread;
 use std::time::Duration;
-use embedded_hal::blocking::delay::DelayMs;
 
 struct StdDelay;
 
@@ -28,7 +22,8 @@ fn main() {
         i2c.get_clock_speed().expect("Failed to get clock speed"),
     );
     let delay = StdDelay;
-    let mut ap33772s = Ap33772s::new_default(i2c, delay).expect("Failed to create AP33772S instance");
+    let mut ap33772s =
+        Ap33772s::new_default(i2c, delay).expect("Failed to create AP33772S instance");
 
     let status = ap33772s.get_status().expect("Failed to get status");
     println!("AP33772S Status: {:?}", status);
